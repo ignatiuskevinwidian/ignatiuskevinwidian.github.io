746ee0576c923c108c0ab2b70dc256c47cd76bd7
ignatiuskevinwidian <ignatiuskevinwidian@gmail.com>
2026-01-27 11:38:54 +0700

    Delete kevin-it-portfolio directory

--- kevin-it-portfolio/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <p>© <?php echo date('Y'); ?> Ignatius Kevin Widian | IT Support & Technical Supervisor</p>
-</footer>
-
-<?php wp_footer(); ?>
-</body>
-</html>
-<button id="darkToggle" class="dark-toggle">🌙 Dark</button>
